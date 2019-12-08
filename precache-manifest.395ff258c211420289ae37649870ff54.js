877f4f8c43f3326cb5de66f7b3229942cfd7e1d1
sergiy khurtik <skhurtik@ukr.net>
2019-12-08 23:11:46 +0200

    Updates

---
+++ precache-manifest.395ff258c211420289ae37649870ff54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba78cc53f43129abb79945732a104f7",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "1089d7597c60739f9b3f",
+    "url": "/goit-react-hw-01-components/static/css/main.eb0c44cb.chunk.css"
+  },
+  {
+    "revision": "72509fa2674d7d8c60a6",
+    "url": "/goit-react-hw-01-components/static/js/2.9b7975e6.chunk.js"
+  },
+  {
+    "revision": "1089d7597c60739f9b3f",
+    "url": "/goit-react-hw-01-components/static/js/main.1081aeb3.chunk.js"
+  },
+  {
+    "revision": "76c0918716ad9d7cd3e5",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.17fd7762.js"
+  }
+]);
